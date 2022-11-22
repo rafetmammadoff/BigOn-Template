57267559a3d9da4ec7e9a054b55e5218a0906d5b
Rafet <professionalraf@gmail.com>
2022-11-23 00:52:33 +0400

    FrontToBack

---
+++ BigOnSolution/BigOn/Controllers/BlogPostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigOn.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
