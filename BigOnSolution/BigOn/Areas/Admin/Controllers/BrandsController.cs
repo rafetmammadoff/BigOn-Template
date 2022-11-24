df7c4d5aaadfdc00b2a090ece1b9d8bdca8ea543
Rafet <professionalraf@gmail.com>
2022-11-24 07:56:00 -0800

    Admin Panel table page

---
+++ BigOnSolution/BigOn/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigOn.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
